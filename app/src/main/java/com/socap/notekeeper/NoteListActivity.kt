64b254b8ad28f267f1f7f2042efa057b5def5d2d
Sophie Cap <sophie.capricorne@gmail.com>
2021-02-16 16:20:01 +0100

    feat: add intent to start NoteListActivity.kt
    fix: launcher activity

--- app/src/main/java/com/socap/notekeeper/NoteListActivity.kt
+++ app/src/main/java/com/socap/notekeeper/NoteListActivity.kt
@@ -1,12 +1,12 @@
 package com.socap.notekeeper
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import com.socap.notekeeper.databinding.ActivityNoteBinding
 import com.socap.notekeeper.databinding.ActivityNoteListBinding
 
 class NoteListActivity : AppCompatActivity() {
@@ -15,15 +15,13 @@ class NoteListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNoteListBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
 
-       /* findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+        binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-        */
 
         initializeDisplayContent()
     }
@@ -31,10 +29,14 @@ class NoteListActivity : AppCompatActivity() {
     private fun initializeDisplayContent() {
         val listNotes: ListView = binding.contentNoteList.listNotes
         val notes : List<NoteInfo> = DataManager.instance.notes
-
         val adapterNotes : ArrayAdapter<NoteInfo> =
             ArrayAdapter(this, android.R.layout.simple_list_item_1, notes)
 
         listNotes.adapter = adapterNotes
+        listNotes.setOnItemClickListener { _, _, position, l ->
+            val intent = Intent(this, NoteActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
