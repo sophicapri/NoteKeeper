1ecc2c6006349f4e244707660952d8f3bb6554b1
Sophie Cap <sophie.capricorne@gmail.com>
2021-03-05 15:14:07 +0100

    feat: update navigation drawer data with Preference

--- app/src/main/java/com/socap/notekeeper/MainActivity.kt
+++ app/src/main/java/com/socap/notekeeper/MainActivity.kt
@@ -5,10 +5,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -42,8 +44,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     private fun configureDrawerLayout() {
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
-        val toggle = ActionBarDrawerToggle(this, drawerLayout, binding.appBarMain.toolbar,
-            R.string.open_navigation_drawer, R.string.close_navigation_drawer)
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, binding.appBarMain.toolbar,
+            R.string.open_navigation_drawer, R.string.close_navigation_drawer
+        )
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
         navView.setNavigationItemSelectedListener(this)
@@ -53,12 +57,34 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onResume() {
         super.onResume()
         noteRecyclerAdapter.notifyDataSetChanged()
+        updateNavHeader()
+    }
+
+    private fun updateNavHeader() {
+        val navigationView = binding.navView
+        val headerView = navigationView.getHeaderView(0)
+        val textUserName = headerView.findViewById<TextView>(R.id.text_user_name)
+        val textEmailAddress = headerView.findViewById<TextView>(R.id.text_email_address)
+
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(this)
+        val userName = sharedPref.getString(
+            getString(R.string.key_user_display_name),
+            getString(R.string.pref_default_display_name)
+        )
+        val emailAddress = sharedPref.getString(
+            getString(R.string.key_user_email_address),
+            getString(R.string.pref_default_email_address)
+        )
+
+        textUserName.text = userName
+        textEmailAddress.text = emailAddress
     }
 
     private fun initializeDisplayContent() {
         recyclerItems = binding.appBarMain.contentMain.listItems
         notesLayoutManager = LinearLayoutManager(this)
-        courseLayoutManager = GridLayoutManager(this, resources.getInteger(R.integer.course_grid_span))
+        courseLayoutManager =
+            GridLayoutManager(this, resources.getInteger(R.integer.course_grid_span))
 
         val notes = DataManager.instance.notes
         noteRecyclerAdapter = NoteRecyclerAdapter(this, notes)
@@ -92,7 +118,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
+        when (item.itemId) {
             R.id.action_settings -> startActivity(Intent(this, SettingsActivity::class.java))
         }
 
@@ -100,7 +126,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
+        when (item.itemId) {
             R.id.nav_notes -> displayNotes()
             R.id.nav_courses -> displayCourses()
             R.id.nav_share -> handleSelection(R.string.nav_share_message)
