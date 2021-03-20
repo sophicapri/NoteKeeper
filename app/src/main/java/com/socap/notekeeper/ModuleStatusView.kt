5780a07cc04b7d350ca3aab61a065640c534f469
Sophie Cap <sophie.capricorne@gmail.com>
2021-03-20 18:29:37 +0100

    add height and width for custom view

--- app/src/main/java/com/socap/notekeeper/ModuleStatusView.kt
+++ app/src/main/java/com/socap/notekeeper/ModuleStatusView.kt
@@ -23,6 +23,7 @@ class ModuleStatusView : View {
     private lateinit var paintOutline: Paint
     private lateinit var paintFill: Paint
     private val radius by lazy { (shapeSize - outlineWidth) / 2 }
+    private var maxHorizontalModules = 0
 
     constructor(context: Context) : super(context) {
         init(null, 0)
@@ -71,7 +72,7 @@ class ModuleStatusView : View {
     private fun setupModuleRectangles() {
         moduleRectangles = Array(moduleStatus.size) { Rect() }
         for (moduleIndex in moduleRectangles.indices) {
-            val x = paddingLeft + moduleIndex * (shapeSize + spacing).toInt()
+            val x = paddingStart + moduleIndex * (shapeSize + spacing).toInt()
             val y = paddingTop
             moduleRectangles[moduleIndex] = Rect(
                 x, y, (x + shapeSize).toInt(), (y + shapeSize).toInt()
@@ -89,6 +90,27 @@ class ModuleStatusView : View {
                 canvas.drawCircle(x, y, radius, paintFill)
             canvas.drawCircle(x, y, radius, paintOutline)
         }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val specWidth = MeasureSpec.getSize(widthMeasureSpec)
+        val availableWidth = specWidth - paddingStart - paddingEnd
+        val horizontalModulesThatCanFit = (availableWidth / (shapeSize + spacing)).toInt()
+        //maxHorizontalModules = Math.min(horizontalModulesThatCanFit, moduleStatus.size)
+        maxHorizontalModules = horizontalModulesThatCanFit.coerceAtMost(moduleStatus.size)
+
+        var desiredWidth: Int = (maxHorizontalModules * (shapeSize + spacing) - spacing).toInt()
+        desiredWidth += paddingStart + paddingEnd
+
+        val rows = (moduleStatus.size - 1 ) / maxHorizontalModules + 1
+        var desiredHeight: Int = (rows * (shapeSize + spacing) - spacing).toInt()
+        desiredHeight += paddingTop + paddingBottom
+
+        val width = resolveSizeAndState(desiredWidth, widthMeasureSpec, 0)
+        val height = resolveSizeAndState(desiredHeight, heightMeasureSpec, 0)
+
+        setMeasuredDimension(width, height)
     }
 
     companion object {
